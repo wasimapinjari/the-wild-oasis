67f6b0f568277dbc2e95079fac18dca86bacda69
Wasim A Pinjari <wasimapinjari@gmail.com>
2023-09-11 16:33:45 +0530

    initial commit

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = import.meta.env.VITE_SUPA_URL;
-const supabaseKey = import.meta.env.VITE_SUPA_KEY;
+export const supabaseUrl = "https://bclhbxxgacnrvvcwujal.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjbGhieHhnYWNucnZ2Y3d1amFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ0MDYwOTMsImV4cCI6MjAwOTk4MjA5M30.1Au7k-P6-bNDFj1ZUg0xw0IX_NtVXLFcyfUTEctwuso";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
